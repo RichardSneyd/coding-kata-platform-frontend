cf0e4b4a0d1f626fd27547f9e0dccecaec5cbb43
Eoan O Dea <odeaeoan@gmail.com>
2022-12-12 15:54:41 +0100

    Redirect from signin to dashboard if logged in

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
-import { Grid, Paper, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Grid } from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import authService from "../services/authService";
 import Login from "./auth/Login";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const user = authService.getUser();
+
+  useEffect(() => {
+    console.log(user);
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [navigate, user]);
+
   return (
     <>
       <Grid container component="main">
