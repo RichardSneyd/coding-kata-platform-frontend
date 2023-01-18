731259c6fd6f2001d1f1f7253fd219fed5e9708c
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2023-01-18 23:31:50 +0000

    added users table

--- src/pages/user/ListUsers.tsx
+++ src/pages/user/ListUsers.tsx
@@ -1,8 +1,66 @@
+import styled from "@emotion/styled";
+import { Add } from "@mui/icons-material";
+import { Typography, Fab, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import dayjs from "dayjs";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppContext, IAppContext } from "../../context/AppContext";
+import { IUser } from "../../interfaces/user";
+
+// Injected styles
+const TitleWrapper = styled("div")`
+display: flex;
+justify-content: space-between;
+`;
+
 const ListUsers = () => {
+  const { members } = useContext(AppContext) as IAppContext;
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const tableFields = ["ID", "Cohort", "Username", "Score"];
+
+  const navigate = useNavigate();
+
   return (
     <>
-      <h2>Users page</h2>
-      <h2>Only an admin should see this</h2>
+      <TitleWrapper>
+        <Typography variant="h1">Users</Typography>
+        <Fab
+          color="primary"
+          aria-label="Add a cohort"
+          component={Link}
+          to="/users"
+        >
+          <Add />
+        </Fab>
+      </TitleWrapper>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {tableFields.map((cell, index) => (
+                <TableCell key={`${index}-${cell}`}>{cell}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {members.map((row) => (
+              <TableRow
+                key={`${row.id}-${row.username}`}
+                hover
+                onClick={() => navigate(`/members/${row.id}`)}
+                style={{ cursor: "pointer" }}
+              >
+                <TableCell>{row.id}</TableCell>
+                <TableCell>{row.cohort?.name}</TableCell>
+                <TableCell>{row.username}</TableCell>
+                <TableCell>{row.score}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
