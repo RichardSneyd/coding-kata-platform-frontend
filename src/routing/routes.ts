2865bb4541e5fd38f1e80eba912a34c4f23b5248
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2023-01-27 09:53:04 +0000

    added user profile and changed ListUsers back from async to .then

--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -23,6 +23,7 @@ import ListSolutions from "../pages/solutions/ListAllSolutions";
 import Solution from "../pages/solutions/Solution";
 
 import ListUsers from "../pages/user/ListUsers";
+import User from "../pages/user/User";
 
 export enum UserRoles {
   UNAUTHED,
@@ -98,6 +99,12 @@ const routes: IRouteType[] = [
     Component: ListUsers,
     authed: UserRoles.ADMIN,
     showInMenuFor: UserRoles.ADMIN,
+  },
+  {
+    name: "User",
+    link: "/users/:id",
+    Component: User,
+    authed: UserRoles.ADMIN,
   },
 
   /**
